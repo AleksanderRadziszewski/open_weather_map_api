d29e655d07726b1b20f650b274a48530242a7f8c
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-03-17 17:26:36 +0100

    creating json file with weather result

--- api/views.py
+++ api/views.py
@@ -2,14 +2,16 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
 import os
+import json
 
 
 class ApiWeatherView(APIView):
 
     def get(self, request):
-        latitude = "52.1347"
-        longitude = "21.0042"
         api_key = os.getenv("api_key")
-        url = f"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&appid={api_key}"
+        cnt="4"
+        url = f"https://api.openweathermap.org/data/2.5/forecast?q=Warsaw,PL&appid={api_key}&cnt={cnt}"
         data = requests.get(url)
+        with open("api/json.txt", "w") as outfile:
+            json.dump(data.json(),outfile)
         return Response(data.json())
