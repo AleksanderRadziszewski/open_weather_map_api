00f25786b8ea6e09855fc52c5396be3f99bbdd48
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-03-29 19:16:35 +0200

    weather for 5 days, exclude daily, alerts, minutely, and current weather

--- api/views.py
+++ api/views.py
@@ -9,10 +9,11 @@ class ApiWeatherView(APIView):
 
     def get(self, request):
         api_key = os.getenv("api_key")
-        lat="33.441792"
-        lon="-94.037689"
-        url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily,current,minutely,alerts&units=metric&appid={api_key}"
+        lat = "33.441792"
+        lon = "-94.037689"
+        url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&" \
+              f"exclude=daily,current,minutely,alerts&units=metric&appid={api_key}"
         data = requests.get(url)
         with open("api/json.txt", "w") as outfile:
-            json.dump(data.json(),outfile, indent=4)
+            json.dump(data.json(), outfile, indent=4)
         return Response(data.json())
