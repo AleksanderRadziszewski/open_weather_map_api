a9cef8896c99dd8eeba1bf74444d8eae95279237
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-04-01 19:18:41 +0200

    deleted serializers.py

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from exercise_1.models import Numbers
-
-
-class NumbersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Numbers
-        fields = ['text', 'number', 'found', 'type']
