c56100455d860ece4b9e1b1149dec279ae9179d5
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-04-02 19:38:42 +0200

    temp field refactor

--- api/models.py
+++ api/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Temperature(models.Model):
     date = models.DateField()
     time = models.TimeField()
-    temp = models.DecimalField(max_digits=4, decimal_places=2)
+    temp = models.FloatField()
