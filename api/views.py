8b5f18087176b7142c5e9861fad6ac7a25eb9f47
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-04-06 14:34:04 +0200

    the correct datimes and objects when temperature was started to rise

--- api/views.py
+++ api/views.py
@@ -13,12 +13,12 @@ class ApiWeatherView(APIView):
         api_key = os.getenv("api_key")
         lat = "52.1356"
         lon = "21.0030"
-        units = "metric"
+        units = "imperial"
         now = datetime.now()
         days = 3
         list_dates = []
         data_requests = []
-        list_temps = []
+        list_objects = []
         for day in range(1, days + 1):
             forecast_date = now - timedelta(days=day)
             dt = int(datetime.timestamp(forecast_date))
@@ -29,13 +29,13 @@ class ApiWeatherView(APIView):
         for day in data_requests:
 
             list_length = len(day["hourly"])
-            for hour in range(1, list_length):
-                converted_hour = datetime.fromtimestamp(day["hourly"][hour]["dt"])
-                list_dates.append(converted_hour.isoformat())
-                if day["hourly"][hour]["temp"] > day["hourly"][hour - 1]["temp"]:
-                    list_temps.append(day["hourly"][hour])
+            for hour in range(list_length - 1):
+                if day["hourly"][hour]["temp"] < day["hourly"][hour+1]["temp"]:
+                    converted_temp_growth = datetime.fromtimestamp(day["hourly"][hour]["dt"])
+                    list_dates.append(converted_temp_growth.isoformat())
+                    list_objects.append(day["hourly"][hour])
         with open("api/json.txt", "w") as outfile:
-            json.dump(list_temps, outfile, indent=4)
+            json.dump(list_objects, outfile, indent=4)
         with open("api/dates_and_hours.txt", "w") as dates_hours:
             for row in list_dates:
                 dates_hours.write(str(row) + "\n")
