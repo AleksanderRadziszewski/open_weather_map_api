a606c6d65d5215258f9f2cccd5908b794cc669b4
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-04-30 13:17:53 +0200

    command description

--- exer_1/views.py
+++ exer_1/views.py
@@ -6,6 +6,9 @@ import json
 from datetime import timedelta, datetime
 
 
+# Przedziały czasowe gdzie odnotowuje się ciągły wzrost temperatury
+
+
 class ApiTempGrowthView(APIView):
 
     def get(self, request):
@@ -18,6 +21,7 @@ class ApiTempGrowthView(APIView):
         delta = timedelta(hours=1)
         data_requests = []
         list_objects = []
+        # Wysyłamy zapytanie o prognoze pogody dla trzech dni poprzedzających dzień dzisiejszy
         for day in range(1, days + 1):
             forecast_date = now - timedelta(days=day)
             dt = int(datetime.timestamp(forecast_date))
@@ -25,13 +29,14 @@ class ApiTempGrowthView(APIView):
                   f"lat={lat}&lon={lon}&dt={dt}&units={units}&appid={api_key}"
             data_request = requests.get(url)
             data_requests.append(data_request.json())
-
+        # obsługujemy każdy dzień
         for day in data_requests:
             list_length = len(day["hourly"])
             local = []
             item = day["hourly"]
 
             for hour in range(1, list_length):
+                # stawiamy warunek by spr czy następuje wzrost temp dla konkretnej godziny
                 if item[hour - 1]["temp"] < item[hour]["temp"]:
                     local.append(item[hour - 1])
                     timestamp_1 = local[-1]["dt"]
@@ -39,6 +44,7 @@ class ApiTempGrowthView(APIView):
                     time_1 = datetime.fromtimestamp(timestamp_1)
                     time_2 = datetime.fromtimestamp(timestamp_2)
                     try:
+                        # sprawdzamy ostatni punkt przedzialu gdzie wzrasta temp i zapisujemy go
                         if time_2 == time_1 + delta and item[hour + 1]["temp"] < item[hour]["temp"]:
                             local.append(item[hour])
                             local_2 = local.copy()
@@ -46,7 +52,7 @@ class ApiTempGrowthView(APIView):
                             local.clear()
                     except IndexError:
                         pass
-
+        # czas rozpoczęcia, czas trwania, temp początkowa i temp końcowa
         for last in list_objects:
             time_start = datetime.fromtimestamp(last[0]["dt"])
             time_end = datetime.fromtimestamp(last[-1]["dt"])
@@ -55,13 +61,14 @@ class ApiTempGrowthView(APIView):
             secs_in_a_hour = 3600
             hours, seconds = divmod(duration_seconds, secs_in_a_hour)
 
-            data = {"start_time_growth": time_start.isoformat(),
-                    "duration_of_growth": f"{hours} hours",
-                    "initial_temp":f"{last[0]['temp']} C",
-                    "end_temp":f"{last[-1]['temp']} C"}
+            data = {"start_time_growth": time_start.isoformat(),  # czas rozpoczęcia wzrostu
+                    "duration_of_growth": f"{hours} hours",  # czas trwania wzrostu w godzinach
+                    "initial_temp": f"{last[0]['temp']} C",  # temperatura początkowa
+                    "end_temp": f"{last[-1]['temp']} C"}  # temperatura końcowa
 
             last.insert(0, data)
 
+        # wrzucamy dane do pliku data.json
         with open("data.json", "w") as outfile:
             json.dump(list_objects, outfile, indent=4)
         return Response(list_objects)
--- exer_2/views.py
+++ exer_2/views.py
@@ -15,6 +15,8 @@ class ApiTempStepView(APIView):
         humidity_avg = 0
         pressure_avg = 0
 
+        # wczytujemy dane z pliku data.json i przypisujemy je do zmiennej list_objects
+
         with open('data.json', 'r') as json_data:
             list_objects = json.load(json_data)
 
@@ -22,19 +24,24 @@ class ApiTempStepView(APIView):
 
             for step in range(2, len(last)):
                 biggest_distinction_last = round(max([last[step]["temp"] - last[step - 1]["temp"]]), 2)
+                #  sprawdzamy gdzie był największy skok temp w przeciągu godziny
                 if biggest_distinction_last:
                     lowest_value_date_last = datetime.fromtimestamp(last[step - 1]["dt"]).isoformat()
                     lowest_value_temp_max_step = last[step]["temp"]
                     temperature_jumps_one_hour.append(
                         (lowest_value_date_last, biggest_distinction_last))
+                    # konwertuejmy czas na czas w formacie ISO 8601
                     time_initial = datetime.fromtimestamp(last[1]["dt"]).isoformat()
                     time_end = datetime.fromtimestamp(last[-1]["dt"]).isoformat()
                     temperature_growth_period.append((time_initial, time_end))
+                    # tworzymy listę wartości wilgotności oraz listę wartości ciśnienia dla poszczególnych pomiarów
                     humidity_data = [last[humidity_hour]["humidity"] for humidity_hour in range(1, len(last))]
                     pressure_data = [last[pressure_hour]["pressure"] for pressure_hour in range(1, len(last))]
+                    # wyliczamy średnią wartość wilgotności i ciśneinia dla okresu wzrostu temp
                     humidity_avg = round(statistics.mean(humidity_data), 2)
                     pressure_avg = round(statistics.mean(pressure_data), 2)
 
+            # temperatura poprzeddnia i maksymalny skok temp
             current_max_value = None
             for s, v in temperature_jumps_one_hour:
                 if current_max_value is not None:
@@ -44,12 +51,15 @@ class ApiTempStepView(APIView):
                     current_max_value = (s, v)
 
             data_step = {"beginning_period": f"{temperature_growth_period[0][0]}",
+                         # początek okresu kiedy nastąpił największy skok
                          "end_period": f"{temperature_growth_period[0][1]}",
-                         "lowest_value_max_step": f"{round(lowest_value_temp_max_step, 2)} C",
-                         "value_of_max_step_one_hour": f"{current_max_value[1]} C",
-                         "initial_time_biggest_step": current_max_value[0],
-                         "humidity_avg": humidity_avg,
-                         "pressure_avg": pressure_avg}
+                         # koniec okresu kiedy nastąpił największy skok
+                         "lowest_value_max_step": f"{round(lowest_value_temp_max_step, 2)} C",  # poprzednia temperatura
+                         "value_of_max_step_one_hour": f"{current_max_value[1]} C",  # przed skokiem, wartość skoku
+                         "initial_time_biggest_step": current_max_value[0],  # data z której pochodzi niższa wartość
+                         "humidity_avg": humidity_avg,  # średnia wilgotność
+                         "pressure_avg": pressure_avg},  # , średnia ciśnienie
+
             list_objects.insert(0, data_step)
 
             return Response(list_objects)
